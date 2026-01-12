da7bb98416ac86c1eebb038dcfc2917a8699f878
augsorn-dev <augsorn.personal@gmail.com>
2026-01-12 16:43:32 +0700

    create b

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 		panic("Error SetUp :: " + err.Error())
 	}
 	// fmt.Println("RUN http://127.0.0.1:" + AppSetUp.FiberV2RunPort)
+
 	if err := AppSetUp.FiberV2.Listen(":" + AppSetUp.FiberV2RunPort); err != nil {
 		log.Fatalf("Server Faoled to start : %v", err)
 	}
